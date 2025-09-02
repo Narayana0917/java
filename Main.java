a73793d1f7fb487011589b7d32d67611583e684c
Narayana0917 <239x1a3248@gprec.ac.in>
2025-09-02 17:24:31 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,17 @@
+package demo;
+
+public class Main {
+
+	static void Display() {
+		System.out.println("Java");
+		System.out.println("SQL");
+		System.out.println("Python");
+	}
+	public static void main(String[] args)
+	{
+		Display();
+		Display();
+		Display();
+	}
+
+}
