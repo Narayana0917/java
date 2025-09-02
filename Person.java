fa6388ef8638603ed822a69b5b64fce85f78fc41
Narayana0917 <239x1a3248@gprec.ac.in>
2025-09-02 17:20:48 +0530

    Add files via upload

---
+++ Person.java
@@ -0,0 +1,14 @@
+package demo;
+
+public class Person {
+		String name;
+		int age;
+		static String Nationality="Indian";
+		public static void main(String[] args) {
+			System.out.println(Nationality);
+			System.out.println(Person.Nationality);
+		}
+
+	}
+
+
